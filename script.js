ae8e4f43274e81c0228aa5aa2ec97fd058905555
amsterjamesdev <jamesryandc8@gmail.com>
2023-05-29 08:58:35 +0800

    first commit

---
+++ script.js
@@ -0,0 +1,17 @@
+let isActive = false;
+
+function Toggle () {
+    if (isActive) {
+        document.querySelector(selectors, '.hamburger').className = 'hamburger active';
+        document.querySelector(selectors, '.shadow').className = 'shadow active';
+        document.querySelector(selectors, '.mobile-nav').className = 'mobile-nav';
+        isActive = false;
+    } else {
+        //Activate Toggle
+        document.querySelector(selectors, '.hamburger').className = 'hamburger active';
+        document.querySelector(selectors, '.shadow').className = 'shadow active';
+        document.querySelector(selectors, '.mobile-nav').className = 'mobile-nav';
+        isActive = true;
+        
+    }
+}
