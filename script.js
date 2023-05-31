13232a479172fa75bcc3ddbd6f57f55360fd42cc
amsterjamesdev <jamesryandc8@gmail.com>
2023-06-01 07:40:35 +0800

    first commit

--- script.js
+++ script.js
@@ -2,8 +2,8 @@ let isActive = false;
 
 function Toggle () {
     if (isActive) {
-        document.querySelector(selectors, '.hamburger').className = 'hamburger active';
-        document.querySelector(selectors, '.shadow').className = 'shadow active';
+        document.querySelector(selectors, '.hamburger').className = 'hamburger';
+        document.querySelector(selectors, '.shadow').className = 'shadow';
         document.querySelector(selectors, '.mobile-nav').className = 'mobile-nav';
         isActive = false;
     } else {
